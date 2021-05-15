08fdcf4d992c274793e114131e45acd0d1052a45
Yuu <yy556023@gmail.com>
2021-05-15 22:11:41 +0800

    2021/05/05～題目11

---
+++ 題目10. 輾轉相除法/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] temp = Console.ReadLine().Split(' ');
+
+            int a = Convert.ToInt32(temp[0]);
+            int b = Convert.ToInt32(temp[1]);
+            int t;
+            while (a != 0 && b != 0)
+            {
+                t = a;
+                a = a % b;
+                b = b % t;
+            }
+
+            Console.WriteLine(a + b);
+        }
+    }
+}
---
+++ 題目11. 矩陣反轉/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            //-----------將陣列設定為幾行幾列設定好-----------
+            string[] inp = Console.ReadLine().Split(' ');
+
+            int a = Convert.ToInt32(inp[0]);
+            int b = Convert.ToInt32(inp[1]);
+            //-------------------------------------------
+
+            //------------把輸出的陣列準備好-----------------
+            string[,] oup = new string[b, a];
+
+
+            //------------把輸入的字串存進oup陣列-------------
+            for (int i = 0; i < a; i++)
+            {
+                inp = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < inp.Length; j++)
+                {
+                    oup[j, i] = inp[j];
+                }
+            }
+            //--------------------------------------------
+
+            //-----------------反轉輸出---------------------
+            for (int i = 0; i < b; i++)
+            {
+                for (int j = 0; j < a; j++)
+                {
+                    if (j == a - 1)
+                    {
+                        Console.WriteLine(oup[i, j]);
+                    }
+                    else
+                    {
+                        Console.Write(oup[i, j] + " ");
+                    }                  
+                }
+            }
+            Console.Read();
+        }
+    }
+}
