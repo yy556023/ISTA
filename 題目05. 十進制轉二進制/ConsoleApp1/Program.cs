4a388a55111295ba27b0022afddb7e211c78fb2c
Yuu <yy556023@gmail.com>
2021-05-17 00:16:09 +0800

    2021/05/17～題目13

---
+++ 題目05. 十進制轉二進制/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            int[] b = new int[8];
+            int i = 0;
+
+
+            if (x < 0)
+            {
+                x += 256;                
+            }
+            
+            while (x != 0)
+            {
+                b[i] = x % 2;
+                x /= 2;
+                i++;
+            }
+
+            Array.Reverse(b);
+
+            foreach (int item in b)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 題目06. 季節判定/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string x = Console.ReadLine();
+
+            switch (x)
+            {
+                case "3":
+                case "4":
+                case "5":
+                    Console.WriteLine("Spring");
+                    break;
+                case "6":
+                case "7":
+                case "8":
+                    Console.WriteLine("Summer");
+                    break;
+                case "9":
+                case "10":
+                case "11":
+                    Console.WriteLine("Autumn");
+                    break;
+                case "12":
+                case "1":
+                case "2":
+                    Console.WriteLine("Winter");
+                    break;
+            }
+        }
+    }
+}
